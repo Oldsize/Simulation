d45257dfeb91da51f53469cee732d9ca71a6432a
Oldsize <164073469+Oldsize@users.noreply.github.com>
2024-05-02 20:47:31 +0300

    Create Cock.java

---
+++ Cock.java
@@ -0,0 +1,13 @@
+package org.example.Herbivores;
+
+import org.example.AnimalType;
+import org.example.Coordinates;
+
+public class Cock extends Herbivore {//🐓🐓🐓
+	public Cock(int creatureSpeed, int creatureHealth, Coordinates coordinates, String entityString, AnimalType animalType) {
+        super(creatureSpeed, creatureHealth, coordinates, entityString, animalType);
+	}
+
+	
+	
+}
