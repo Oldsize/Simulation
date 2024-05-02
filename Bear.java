06f94459a7ed65a79f7a28f493a2d7134a1bf838
Oldsize <164073469+Oldsize@users.noreply.github.com>
2024-05-02 20:45:54 +0300

    Create Bear.java

---
+++ Bear.java
@@ -0,0 +1,14 @@
+package org.example.Predators;
+
+import org.example.AnimalType;
+import org.example.Coordinates;
+
+public class Bear extends Predator {
+    public Bear(int creatureSpeed, int creatureHealth, Coordinates coordinates, String entityString, int predatorStrength, AnimalType animalType) {
+        super(creatureSpeed, creatureHealth, coordinates, entityString, predatorStrength, animalType);
+    }
+
+    
+
+
+}
