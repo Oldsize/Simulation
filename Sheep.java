831e7c209c73a0c6823e62d973d408355ef950b8
Oldsize <164073469+Oldsize@users.noreply.github.com>
2024-05-02 20:46:32 +0300

    Create Sheep.java

---
+++ Sheep.java
@@ -0,0 +1,12 @@
+package org.example.Herbivores;
+
+import org.example.AnimalType;
+import org.example.Coordinates;
+
+public class Sheep extends Herbivore {
+    public Sheep(int creatureSpeed, int creatureHealth, Coordinates coordinates, String entityString, AnimalType animalType) {
+        super(creatureSpeed, creatureHealth, coordinates, entityString, animalType);
+    }
+
+    
+}
