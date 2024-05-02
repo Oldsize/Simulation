157b337cf0978d3420c4f038895109d4372e97f0
Oldsize <164073469+Oldsize@users.noreply.github.com>
2024-05-02 20:44:51 +0300

    Create Wolf.java

---
+++ Wolf.java
@@ -0,0 +1,13 @@
+package org.example.Predators;
+
+import org.example.AnimalType;
+import org.example.Coordinates;
+
+public class Wolf extends Predator {
+
+	public Wolf(int creatureSpeed, int creatureHealth, Coordinates coordinates, String entityString, int predatorStrength, AnimalType animalType) {
+		super(creatureSpeed, creatureHealth, coordinates, entityString, predatorStrength, animalType);
+	}
+	
+	
+}
