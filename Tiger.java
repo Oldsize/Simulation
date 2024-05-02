595eb4f22a2fece5666fe3040afe787267b7aeeb
Oldsize <164073469+Oldsize@users.noreply.github.com>
2024-05-02 20:45:12 +0300

    Create Tiger.java

---
+++ Tiger.java
@@ -0,0 +1,14 @@
+package org.example.Predators;
+
+import org.example.AnimalType;
+import org.example.Coordinates;
+
+public class Tiger extends Predator {
+	
+	public Tiger(int creatureSpeed, int creatureHealth, Coordinates coordinates, String entityString, int predatorStrength, AnimalType animalType) {
+		super(creatureHealth, creatureHealth, coordinates, entityString, predatorStrength, animalType);
+	}
+
+
+	
+}
