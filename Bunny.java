a6a43b331b0c63914218a07fdd22787bcba77530
Oldsize <164073469+Oldsize@users.noreply.github.com>
2024-05-02 20:48:00 +0300

    Create Bunny.java

---
+++ Bunny.java
@@ -0,0 +1,13 @@
+package org.example.Herbivores;
+
+
+import org.example.AnimalType;
+import org.example.Coordinates;
+
+public class Bunny extends Herbivore {
+    public Bunny(int creatureSpeed, int creatureHealth, Coordinates coordinates, String entityString, AnimalType animalType) {
+        super(creatureSpeed,creatureHealth, coordinates, entityString, animalType);
+    }
+
+
+}
